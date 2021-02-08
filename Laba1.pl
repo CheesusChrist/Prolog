3fd3198dec31ebe8a06efdf931cd8580d9f12049
Daniel <62421247+CheesusChrist@users.noreply.github.com>
2021-02-08 16:41:29 +0300

    Задание 2
    Построить базу данных Prolog отношения «является родителем», пример доступен по ссылке выше. Провести несколько
    запросов к терминалу Prolog, снимки экрана трех результатов прикрепить в отчет. Построить предикат, который children(X), который выводит всех детей X

--- Laba1.pl
+++ Laba1.pl
@@ -30,4 +30,38 @@ woman(gertrude).
 men(X):-man(X), write(X), write(" "), fail.
 women(X):-woman(X), write(X), write(" "), fail.
 
+parent(wolfgang,petra).
+parent(helga,petra).
+
+parent(tomas,klaus).
+parent(tomas,gisela).
+parent(tomas,frank).
+parent(tomas,ingrid).
+parent(maria,klaus).
+parent(maria,gisela).
+parent(maria,frank).
+parent(maria,ingrid).
+
+parent(uwe,andreas).
+parent(uwe,michael).
+parent(elke,andreas).
+parent(elke,michael).
+
+parent(petra,hans).
+parent(petra,adolf).
+parent(petra,walter).
+parent(klaus,hans).
+parent(klaus,adolf).
+parent(klaus,walter).
+
+parent(ingrid,anna).
+parent(ingrid,sabine).
+parent(ingrid,gertrude).
+parent(andreas,anna).
+parent(andreas,sabine).
+parent(andreas,gertrude).
+
+child(X,Y):-parent(Y,X).
+children(X):-child(Y,X), write(Y), write(" "), fail.
+
 :- program.
