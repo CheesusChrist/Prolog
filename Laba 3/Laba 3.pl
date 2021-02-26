76374760610255eb6d7a19497ff91436fe11969a
Daniel <62421247+CheesusChrist@users.noreply.github.com>
2021-02-26 23:09:16 +0300

    Задание 4
    Реализовать предикат fact(N,X), где X – это факториал первого
    аргумента с помощью рекурсии вниз.

--- Laba 3/Laba 3.pl
+++ Laba 3/Laba 3.pl
@@ -7,3 +7,7 @@ max(_,_,U,U).
 
 fact(1,1):-!.
 fact(N,X):- N1 is N-1, fact(N1,X1), X is X1*N.
+
+fact(1, CurX, CurX):-!.
+fact(N,CurX,X):-CurX1 is CurX*N, N1 is N-1, fact(N1, CurX1, X).
+factorial(N,X):-fact(N,1,X).
