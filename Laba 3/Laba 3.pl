8620d6bd2be50d3dbca937bd48ae1ac575bb61c4
Daniel <62421247+CheesusChrist@users.noreply.github.com>
2021-02-26 23:10:00 +0300

    Задание 5
    Реализовать предикат fib(N,X), где X – число Фибоначчи с
    номером N, причем 1 и 2 элемент равны 1 с помощью рекурсии вверх.

--- Laba 3/Laba 3.pl
+++ Laba 3/Laba 3.pl
@@ -11,3 +11,8 @@ fact(N,X):- N1 is N-1, fact(N1,X1), X is X1*N.
 fact(1, CurX, CurX):-!.
 fact(N,CurX,X):-CurX1 is CurX*N, N1 is N-1, fact(N1, CurX1, X).
 factorial(N,X):-fact(N,1,X).
+
+fib(0,0):-!.
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):- N1 is N-1, N2 is N-2, fib(N1,X1), fib(N2,X2), X is X1+X2.
