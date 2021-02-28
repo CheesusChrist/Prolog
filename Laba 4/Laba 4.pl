b0b631a8ae3a207416c50e32f23166457534a5e5
Daniel <62421247+CheesusChrist@users.noreply.github.com>
2021-02-28 13:35:34 +0300

    Задание 2
    Реализовать предикат sum_list_down(+List, ?Summ), который проверяет, является ли Summ суммой элементов списка или записывает в
    эту переменную сумму элементов. Для построения воспользоваться
    рекурсией вниз. Реализовать программу, читающую список, считающую
    сумму элементов и выводящую сумму на экран, с использованием
    предикатов из задания 1 и построенного предиката sum_list_down /2.

--- Laba 4/Laba 4.pl
+++ Laba 4/Laba 4.pl
@@ -3,3 +3,6 @@ read_list(N,[Head|Tail]):-read(Head),N1 is N-1,read_list(N1,Tail).
 
 write_list([]):-!.
 write_list([H/T]):-write(H),write_list(T).
+
+sum_ls_down([], Sum, Sum) :- !.
+sum_ls_down([Head|Tail], CurSum, Sum) :- CurSum1 is CurSum + Head, sum_ls_down(Tail, CurSum1, Sum).
