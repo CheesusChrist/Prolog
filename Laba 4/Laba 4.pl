2f026b768c91fe07fb38e79f1eb95440d8dbde1e
Daniel <62421247+CheesusChrist@users.noreply.github.com>
2021-02-28 16:03:17 +0300

    Изменение задания 1
    Добавлен предикат append_element для добавления эл-та в список

--- Laba 4/Laba 4.pl
+++ Laba 4/Laba 4.pl
@@ -1,8 +1,12 @@
-read_list(0,[]):-!.
-read_list(N,[Head|Tail]):-read(Head),N1 is N-1,read_list(N1,Tail).
+append_element([], X, X).
+append_element([H|T1], X, [H|T2]) :- append_element(T1, X, T2).
 
-write_list([]):-!.
-write_list([H/T]):-write(H),write_list(T).
+read_list(N, L) :- read_list(N, 0, [], L), !.
+read_list(N, N, L, L) :- !.
+read_list(N, CurN, CurL, L) :- CurN1 is CurN + 1, read(X), append_element(CurL, [X], CurL1), read_list(N, CurN1, CurL1, L).
+
+write_list([]) :- !.
+write_list([H|T]) :- write(H), write(" "), write_list(T).
 
 sum_ls_down([], Sum, Sum) :- !.
 sum_ls_down([Head|Tail], CurSum, Sum) :- CurSum1 is CurSum + Head, sum_ls_down(Tail, CurSum1, Sum).
