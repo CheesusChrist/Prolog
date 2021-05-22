9f02037c17441e341f147e8a89f69d7eae873099
Daniel <62421247+CheesusChrist@users.noreply.github.com>
2021-05-22 18:01:43 +0300

    Задание 1.1
    Дан файл. Прочитать из файла строки и вывести длину наибольшей
    строки.

---
+++ Laba 8/Laba 8.pl
@@ -0,0 +1,35 @@
+read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
+
+read_list_str(List, LengthList):-read_str(A,N,Flag),read_list_str([A],List,[N],LengthList,Flag).
+read_list_str(List,List,LengthList, LengthList,1):-!.
+read_list_str(Cur_list,List,CurLengthList,LengthList,0):-
+read_str(A,N,Flag),append(Cur_list,[A],C_l),
+append(CurLengthList, [N], NewLengthList),read_list_str(C_l,List,NewLengthList,LengthList,Flag).
+
+max(List, MaxEl):- max(List, 0, MaxEl).
+max([],CurMax, CurMax):- !.
+max([H|T], CurMax, X):- H > CurMax, NewMax is H, max(T,NewMax,X), !.
+max([_|T], CurMax, X):- max(T, CurMax, X).
+
+append1([],List2,List2).
+append1([H1|T1],List2,[H1|T3]):-append1(T1,List2,T3).
+
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+write_list_str([]):-!.
+write_list_str([H|T]):-write_str(H),nl,write_list_str(T).
+
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
+reverse(A, Z) :- reverse(A,Z,[]).
+reverse([],Z,Z).
+reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
+
+%__1_1__
+pr8_1:- see('c:/Users/Daniel/Documents/GitHub/Prolog/Lab_8/lab.txt'),read_list_str(List, Lenght),seen,max(Lenght, Max), write(Max).
+
